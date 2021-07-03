da685927b2ce43e8324889e0a8eda546e5baaf24
dheeru-karanam <dheerajkaranam1@gmail.com>
2021-07-04 01:31:46 +0530

    Few more codes to LinkedList
    Added code to detect Loop in a linkedList and code to check if the linkedList is palindrome

---
+++ Is_Palindrome.cpp
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+ // } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        //Your code here
+        struct Node *ptr , *qtr;
+        //ptr = (struct Node *)malloc(sizeof(struct Node));
+        qtr = (struct Node *)malloc(sizeof(struct Node));
+        ptr = qtr = head;
+        int arr[100000],i=0;
+        while(qtr){
+            arr[i++] = qtr->data;
+            qtr = qtr->next;
+        }
+        int length = i;
+        for(i=0;i<length;i++){
+            if(arr[i]!=arr[length-i-1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
